9ae59921d904e6acd55b956c4a560218dc89a7ba
Kyle Honeycutt <coinables@gmail.com>
2017-06-18 21:26:21 -0700

    add comments
    add comments

--- index.php
+++ index.php
@@ -27,18 +27,22 @@ function bittrexbuy($apikey, $apisecret, $symbol, $quant, $rate){
     return $obj;
 }
 
-
+//fetch top 50 cryptos by marketcap
 $cnmkt = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
 $fgc = json_decode(file_get_contents($cnmkt), true);
 $counter = 0;
 for($i=0;$i<50;$i++){
-    if($counter < 4){
+    if($counter < 3){
+        //check percentage change over last 7 days
         $percCng = $fgc[$i]["percent_change_7d"];
         if($percCng < 4 && $percCng > -4){
             $symbol = $fgc[$i]["symbol"];
             $cost = $fgc[$i]["price_btc"];
+            //fetch bittrex btc balance
             $balance = bittrexbalance($apikey, $apisecret);
+            //calc 1/5th of available
             $fifthBal = $balance / 5;
+            //calc how much coin to buy
             $amounttobuy = $fifthBal / $cost;
             $buy = bittrexbuy($apikey, $apisecret, $symbol, $amounttobuy, $cost);
             print_r($buy);
